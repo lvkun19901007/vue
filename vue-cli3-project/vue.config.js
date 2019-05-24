baf4705592be7af2713c21537d995f1317940557
lvkun <lvkun107@foxmail.com>
2019-05-24 17:33:45 +0800

    vue.config.js

--- vue-cli3-project/vue.config.js
+++ vue-cli3-project/vue.config.js
@@ -1,7 +1,23 @@
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
+  outputDir: "dist",
+  filenameHashing: false, // 配置打包后文件名是否需要hash值
   devServer: {
     port: 8081,
     host: "localhost",
     open: true
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|.\css/, //匹配文件名
+            threshold: 10240, //对超过10k的数据压缩
+            deleteOriginalAssets: false //不删除源文件
+          })
+        ]
+      };
+    }
   }
 };
