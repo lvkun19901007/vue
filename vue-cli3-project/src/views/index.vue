6e64c72c5bd62275ef2d7c80adc6d9d68af05d87
lvkun <lvkun107@foxmail.com>
2019-05-24 17:33:45 +0800

    less

---
+++ vue-cli3-project/src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="morning">早上好</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data () {
+    return {
+      newDate: ''
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.morning {
+  .fs(32);
+}
+</style>
--- vue-cli3-project/vue.config.js
+++ vue-cli3-project/vue.config.js
@@ -1,7 +1,48 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+const path = require("path");
 module.exports = {
+  outputDir: "dist",
+
+  // 配置打包后文件名是否需要hash值
+  filenameHashing: true,
+
   devServer: {
     port: 8081,
     host: "localhost",
     open: true
+  },
+
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|.\css/, //匹配文件名
+            threshold: 10240, //对超过10k的数据压缩
+            deleteOriginalAssets: false //不删除源文件
+          })
+        ]
+      };
+    }
+  },
+  chainWebpack: config => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach(type =>
+      addStyleResource(config.module.rule("less").oneOf(type))
+    );
+  },
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.resolve(__dirname), "src/style/mixin.less"]
+    }
   }
 };
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [path.resolve(__dirname, "./src/style/mixin.less")]
+    });
+}
