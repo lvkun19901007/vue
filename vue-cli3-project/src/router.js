29626511fe46cbecab8e9bd3c3d39ded846a079b
lvkun <lvkun107@foxmail.com>
2019-07-12 22:04:44 +0800

    20190712

--- vue-cli3-project/src/router.js
+++ vue-cli3-project/src/router.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "./views/index.vue";
@@ -13,14 +14,5 @@ export default new Router({
       name: "Index",
       component: Index
     }
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // }
   ]
 });
--- vue-cli3-project/src/views/index.vue
+++ vue-cli3-project/src/views/index.vue
@@ -5,13 +5,34 @@
 </template>
 
 <script>
+// @ts-check
 export default {
   name: "Index",
   data () {
     return {
       newDate: ''
     }
-  }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      /**
+       * @typedef {Object} person a peron.
+       * @property {string} name The person's name.
+       * @property {number} age The person's age.
+       * @property {Function} sayName A function that alerts the person's name.
+       */
+      const person = {
+        name: 23,
+        age: '32',
+        sayName () {
+          alert(this.name)
+        }
+      }
+    }
+  },
 };
 </script>
 <style lang="less" scoped>
